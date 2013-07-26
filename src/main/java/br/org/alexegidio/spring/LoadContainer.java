dc8c1eca95db02d27ff1b9b9864c2da38e1dca60
Alex Egidio <alexegidio@yahoo.com.br>
2013-07-26 11:40:19 -0300

    Criando consumidores Listener e producers para spring

--- src/main/java/br/org/alexegidio/spring/LoadContainer.java
+++ src/main/java/br/org/alexegidio/spring/LoadContainer.java
@@ -3,6 +3,7 @@ package br.org.alexegidio.spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 public class LoadContainer {
 
 	public static void main(String[] args) {
