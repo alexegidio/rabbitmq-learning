d9e37012674d61721cce885468a54f2b03bdd269
Alex Egidio <alexegidio@yahoo.com.br>
2013-07-28 10:36:32 -0300

    Criando consumidor com confirmationCallBack

--- src/main/java/br/org/alexegidio/spring/confirm/SpringRabbitConfirmSender.java
+++ src/main/java/br/org/alexegidio/spring/confirm/SpringRabbitConfirmSender.java
@@ -4,9 +4,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.core.RabbitTemplate.ReturnCallback;
+import org.springframework.amqp.rabbit.core.RabbitTemplate.ConfirmCallback;
+import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -19,14 +19,17 @@ public class SpringRabbitConfirmSender {
 		RabbitTemplate aTemplate = (RabbitTemplate) context.getBean("confirmTemplate");
 
 		aTemplate.setMandatory(true);
-		aTemplate.setReturnCallback(new ReturnCallback() {
+		aTemplate.setConfirmCallback(new ConfirmCallback() {
 
-			public void returnedMessage(Message message, int replyCode, String replyText, String exchange,
-					String routingKey) {
-				System.out.println("Message " + message + "confirmed" + "reply" + replyCode);
+			public void confirm(CorrelationData correlationData, boolean ack) {
+				if (ack) {
+					System.out.println("Confirmation received " + correlationData + " at " + new Date());
+				} else {
+					System.out.println("Msg not received " + correlationData + " at " + new Date());
+				}
+
 			}
 		});
-
 		for (int i = 0; i < 10; i++) {
 			String message = new String("Message # " + i + " on ");
 			aTemplate.convertAndSend("confirm", message + new Date());
